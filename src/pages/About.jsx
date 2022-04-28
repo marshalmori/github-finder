10a4eeefabb98500bd38395a003e654d7d8317d3
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-04-27 22:33:50 -0300

    Create Home, About and NotFound pages and routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,41 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-4xl mb-4 mt-10">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vestibulum
+        dolor eu feugiat consequat. Proin a congue metus. Integer elit sem,
+        pretium at nulla ut, accumsan congue arcu. In at porttitor orci. Nullam
+        vestibulum consectetur magna nec aliquet. Fusce tincidunt rutrum eros, a
+        varius enim dignissim eu. Nam ultrices ac neque vel finibus. Nullam
+        dapibus sit amet mi quis consequat. Nullam varius venenatis lorem, quis
+        pellentesque neque. Donec porttitor metus metus, eget vehicula diam
+        pulvinar at. In varius ullamcorper dui in pellentesque. Suspendisse quis
+        mi tincidunt, auctor dolor vitae, porttitor velit.
+      </p>
+      <p className="mb-4 text-2xl font-light">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vestibulum
+        dolor eu feugiat consequat. Proin a congue metus. Integer elit sem,
+        pretium at nulla ut, accumsan congue arcu. In at porttitor orci. Nullam
+        vestibulum consectetur magna nec aliquet. Fusce tincidunt rutrum eros, a
+        varius enim dignissim eu. Nam ultrices ac neque vel finibus. Nullam
+        dapibus sit amet mi quis consequat. Nullam varius venenatis lorem, quis
+        pellentesque neque. Donec porttitor metus metus, eget vehicula diam
+        pulvinar at. In varius ullamcorper dui in pellentesque. Suspendisse quis
+        mi tincidunt, auctor dolor vitae, porttitor velit.
+      </p>
+
+      <p className="text-lg text-gray-400">
+        Version: <span className="text-white">1.0.0</span>
+      </p>
+      <p className="text-lg text-gray-400 mb-10">
+        Layout By:{" "}
+        <a className="text-white" href="https://twitter.com/marshalmori">
+          Marshal Mori Cavalheiro
+        </a>
+      </p>
+    </>
+  );
+}
+
+export default About;
