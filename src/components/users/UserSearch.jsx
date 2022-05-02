1a2c7ecb1c489bc728c5405f76b02f1b50253531
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2022-05-02 16:57:18 -0300

    Create search Users

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -4,23 +4,24 @@ import GithubContext from "../../context/github/GithubContext";
 function UserSearch() {
   const [text, setText] = useState("");
 
-  const { users } = useContext(GithubContext);
+  const { users, searchUsers } = useContext(GithubContext);
 
   const handleChange = (e) => setText(e.target.value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     if (text === "") {
       alert("Please enter something");
     } else {
-      // @todo - search users
+      searchUsers(text);
 
       setText("");
     }
   };
 
   return (
-    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols2 mb-8 gap-8">
+    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols2 mb-8 mt-10 gap-8">
       <div>
         <form onSubmit={handleSubmit} action="">
           <div className="form-control">
